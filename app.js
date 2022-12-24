a4383df01f61e6accbe85b629eaa7c7692a60626
mmieses01 <manuel.mieses48@gmail.com>
2022-12-24 07:11:59 -0800

    Add gitignore, Procfile and Update Port

--- app.js
+++ app.js
@@ -171,6 +171,11 @@ app.get("/:customListName", function(req, res) {
 
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Running in port 3000");
 });
